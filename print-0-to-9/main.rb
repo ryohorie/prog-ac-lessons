15400fd4221f9da640b89d484bc532a1c6381b0c
ryohorie <develop@ryohorie.com>
2020-05-01 15:47:37 +0900

    add new line

--- print-0-to-9/main.rb
+++ print-0-to-9/main.rb
@@ -1,3 +1,8 @@
 for i in 0..9 do
   puts i
 end
+
+# または
+(0..9).each do |num|
+  puts num
+end
---
+++ wow-in-3/main.js
@@ -0,0 +1,7 @@
+for(let i = 1; i <= 50; i++) {
+  if(i % 3 == 0 || i.toString().indexOf('3') >= 0){
+    console.log('WOW')
+  }else{
+    console.log(i)
+  }
+}
